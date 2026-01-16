4f88635bf0e35d5834f4c4d144a58935b15e3215
Riqis24 <riqisaputra24@gmail.com>
2026-01-16 10:10:48 +0700

    Switch database to filess.io

--- config/database.php
+++ config/database.php
@@ -57,9 +57,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ],
         ],
 
         'mariadb' => [
